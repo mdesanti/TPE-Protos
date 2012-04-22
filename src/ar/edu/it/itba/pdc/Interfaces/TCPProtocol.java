ae19730205e9da55d349e0a861cdcf8f61541581
mdesanti <mdesanti90@gmail.com>
2012-04-22 11:45:38 -0300

    Advancing in threads implementation

--- src/ar/edu/it/itba/pdc/Interfaces/TCPProtocol.java
+++ src/ar/edu/it/itba/pdc/Interfaces/TCPProtocol.java
@@ -3,8 +3,17 @@ package ar.edu.it.itba.pdc.Interfaces;
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
 
+import ar.edu.it.itba.pdc.Implementations.utils.TCPServerSelector;
+
 public interface TCPProtocol {
-      void handleAccept(SelectionKey key) throws IOException;
-      void handleRead(SelectionKey key) throws IOException;
-      void handleWrite(SelectionKey key) throws IOException;
+	
+      public void handleAccept(SelectionKey key) throws IOException;
+      
+      public void handleRead(SelectionKey key) throws IOException;
+      
+      public void handleWrite(SelectionKey key) throws IOException;
+      
+      public void setWorker(ProxyWorker worker);
+      
+      public void setCaller(TCPServerSelector caller);
 }
