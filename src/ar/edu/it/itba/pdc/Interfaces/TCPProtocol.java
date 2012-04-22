b967f74104584ef799b000b450f01a1ffddf831a
mdesanti <mdesanti90@gmail.com>
2012-04-22 11:56:30 -0300

    Server and worker finished? Next step, client

--- src/ar/edu/it/itba/pdc/Interfaces/TCPProtocol.java
+++ src/ar/edu/it/itba/pdc/Interfaces/TCPProtocol.java
@@ -1,7 +1,11 @@
 package ar.edu.it.itba.pdc.Interfaces;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+import java.util.Map;
+import java.util.Queue;
 
 import ar.edu.it.itba.pdc.Implementations.utils.TCPServerSelector;
 
@@ -11,7 +15,7 @@ public interface TCPProtocol {
       
       public void handleRead(SelectionKey key) throws IOException;
       
-      public void handleWrite(SelectionKey key) throws IOException;
+      public void handleWrite(SelectionKey key, Map<SocketChannel, Queue<ByteBuffer>> map) throws IOException;
       
       public void setWorker(ProxyWorker worker);
       
