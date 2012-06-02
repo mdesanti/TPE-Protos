65bb85014c7827e06f1c7aaaff218d188eb6fce7
Federico <federico@federico-MS-7369.(none)>
2012-06-02 12:41:40 -0300

    Fixing Buffer issues

--- src/ar/edu/it/itba/pdc/v2/implementations/monitor/MonitorDecoder.java
+++ src/ar/edu/it/itba/pdc/v2/implementations/monitor/MonitorDecoder.java
@@ -17,35 +17,40 @@ public class MonitorDecoder implements ConnectionDecoder {
 	}
 
 	public String decode(String s) {
-		String[] args = s.split(" ");
-		String method = args[0];
-		String option = args[1];
-		String filter = args[2];
-		// System.out.println(method + "!" + option + "!" + filter + "!");
-		if (!method.equals("GET"))
-			return options.get("BAD_REQUEST");
-		if (option.equals("BYTES"))
-			return analizeComandoBytes(filter);
-		if (option.equals("COUNT"))
-			return analizeComandoCount(filter);
-		if (option.equals("CONS"))
-			return analizeComandoCons(filter);
 
-		return options.get("BAD_REQUEST");
+		try {
+			String[] args = s.split(" ");
+			String method = args[0];
+			String option = args[1];
+			String filter = args[2];
+
+			if (!method.equals("GET"))
+				return options.get("BAD_REQUEST");
+			if (option.equals("BYTES"))
+				return analizeComandoBytes(filter);
+			if (option.equals("COUNT"))
+				return analizeComandoCount(filter);
+			if (option.equals("CONS"))
+				return analizeComandoCons(filter);
+
+			return options.get("BAD_REQUEST");
+		} catch (Exception e) {
+			return options.get("BAD_REQUEST");
+		}
 	}
 
 	private String analizeComandoBytes(String filter) {
 		try {
 
-			if (filter.equals("ALL"))
+			if (filter.equals("ALL\n"))
 				return this.options.get("OK") + "\ncomando-bytes(ALL)="
-						+ storage.getTotalBytes();
-			if (filter.equals("CP"))
+						+ storage.getTotalBytes() + "\n";
+			if (filter.equals("CP\n"))
 				return this.options.get("OK") + "\n comando-bytes(CP)="
-						+ storage.getClientProxyBytes();
-			if (filter.equals("PS"))
+						+ storage.getClientProxyBytes() + "\n";
+			if (filter.equals("PS\n"))
 				return this.options.get("OK") + "\n comando-bytes(CS)="
-						+ storage.getProxyServersBytes();
+						+ storage.getProxyServersBytes() + "\n";
 
 			return this.options.get("BAD_REQUEST");
 		} catch (NullPointerException e) {
@@ -56,23 +61,23 @@ public class MonitorDecoder implements ConnectionDecoder {
 	}
 
 	private String analizeComandoCount(String filter) {
-		if (filter.equals("BLOCK"))
+		if (filter.equals("BLOCK\n"))
 			return this.options.get("OK") + "\ncomando-count(BLOCK)="
-					+ storage.getBlocks();
-		if (filter.equals("TRANS"))
+					+ storage.getBlocks() + "\n";
+		if (filter.equals("TRANS\n"))
 			return this.options.get("OK") + "\ncomando-count(TRANS)="
-					+ storage.getTransformations();
+					+ storage.getTransformations() + "\n";
 
 		return this.options.get("BAD_REQUEST");
 	}
 
 	private String analizeComandoCons(String filter) {
-		if (filter.equals("C"))
+		if (filter.equals("C\n"))
 			return this.options.get("OK") + "\ncomando-cons(C)="
-					+ storage.getClientOpenConections();
-		if (filter.equals("S"))
+					+ storage.getClientOpenConections() + "\n";
+		if (filter.equals("S\n"))
 			return this.options.get("OK") + "\ncomando-count(S)="
-					+ storage.getServersOpenConections();
+					+ storage.getServersOpenConections() + "\n";
 
 		return this.options.get("BAD_REQUEST");
 
@@ -83,9 +88,8 @@ public class MonitorDecoder implements ConnectionDecoder {
 		options.put("LOG_IN_OK", "200 - Welcome\n");
 		options.put("LOGIN_ERROR", "400 - Wrong username and/or password\n");
 		options.put("BAD_REQUEST", "401 - Bad request\n");
-		options
-				.put("PARAM_MATCH",
-						"401 - The specified command does not match the used command\n");
+		options.put("PARAM_MATCH",
+				"401 - The specified command does not match the used command\n");
 		options.put("OK", "200 - OK");
 	}
 
